6543d6950f7eb04c84d1ff50f11ed2d50185a072
Edson Orivaldo Lessa Junior <84724309+edson-lessa-jr@users.noreply.github.com>
2022-11-29 20:22:04 -0300

    Atividade4 atualizado

--- src/atividade4/CalculoIPI.java
+++ src/atividade4/CalculoIPI.java
@@ -1,6 +1,6 @@
 package atividade4;
 
-import javax.swing.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalculoIPI {
@@ -12,23 +12,28 @@ public class CalculoIPI {
             final double alicotaIPI = 24.75;
             for (int i = 0; i < pecas.length; i++) {
                 System.out.println("Informe os dados da peça com código " + (i + 1));
-
                 /* na primeira posição 0 eu quero a quantidade */
-                System.out.println("Informe a quantidade");
-                Scanner scanner = new Scanner(System.in);
-                pecas[i][0] = scanner.nextDouble();
+                pecas[i][0] = coletaDados("Informe a quantidade");
                 /* na segunda posição tem o valor */
-                System.out.println("Informe o valor");
-                pecas[i][1] = scanner.nextDouble();
+                pecas[i][1] = coletaDados("Informe o valor");
             }
             double valorFinal = 0;
             for (int i = 0; i < pecas.length; i++) {
                 valorFinal += pecas[i][0] * pecas[i][1];
             }
-            System.out.println("Valor dos produtos "+ valorFinal);
-            System.out.println("Valor final com IPI "+valorFinal *(alicotaIPI/100+1));
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Valor dos produtos " + valorFinal);
+            System.out.println("Valor final com IPI " + valorFinal * (alicotaIPI / 100 + 1));
+        } catch (InputMismatchException e) {
+            System.out.println("Dados informado é inválido");
+        } catch (Exception e){
+            System.out.println("Houve um erro não identificado: "+e.getMessage());
+
         }
+    }
+
+    private static double coletaDados(String mensagem) {
+        System.out.println("Informe a quantidade");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
     }
 }
